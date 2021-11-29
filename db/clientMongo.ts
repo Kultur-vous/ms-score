a31c3618eed5969db511b2f8f9457b20b9530d37
marth154 <37938187+marth154@users.noreply.github.com>
2021-11-29 22:09:06 +0100

    client mongo

--- db/clientMongo.ts
+++ db/clientMongo.ts
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 require('dotenv').config();
 //const mongoUri =
-  //`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@users.pnxzv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
+  //`mongodb+srv://${process.env.MONGO_USERNAME}:${process.env.MONGO_PASS}@scores.pnxzv.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`;
   const mongoUri =
   `mongodb+srv://admin:7szPPLTsFXd3Yxs@scores.tnzut.mongodb.net/scores?retryWrites=true&w=majority`;
   
