705f240b37b68ca88ec48ac4c00547cb5099800f
mariegautron <marigaut2@gmail.com>
2021-12-02 00:51:53 +0100

    cors

--- app.ts
+++ app.ts
@@ -1,17 +1,34 @@
 const express = require("express");
-import { ScoreService } from "./service/score";
 import mongoConnection from "./db/clientMongo";
-import Score, {DataScore} from "./interface/score";
-import jwt from "jsonwebtoken";
 import auth from "./middleware/auth";
+import { ScoreService } from "./service/score";
+const cors = require("cors");
 
 const app = express();
 const _mongoConnection = mongoConnection;
 const scoreService = new ScoreService();
 require("dotenv").config();
+app.use(cors());
 
 //const bodyParser = require("body-parser");
 app.use(express.json());
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type, Authorization, email, id"
+  );
+
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
 
 app.get("/", async (req: any, res: any) => {
   res.setHeader("Content-Type", "application/json");
@@ -19,25 +36,28 @@ app.get("/", async (req: any, res: any) => {
 });
 
 app.post("/addScore", auth, async (req: any, res: any) => {
+  console.log(req.body, req.headers);
   res.setHeader("Content-Type", "application/json");
-  if(!req.headers.id) {res.status(400).send({error: "Il manque un paramètre dans le headers"})}
+  if (!req.headers.id) {
+    res.status(400).send({ error: "Il manque un paramètre dans le headers" });
+  }
   const createScore = await scoreService.addScore(req.body, req.headers.id);
-  res.status(200).send(createScore)
+  res.status(200).send(createScore);
 });
 
-app.get('/scores', async (req: any, res: any) => {
+app.get("/scores", async (req: any, res: any) => {
   res.setHeader("Content-Type", "application/json");
-  
-  if(!req.headers.id) {
-    res.status(400).send({error: "Il manque un paramètre dans le headers"})
-  }
-  const getScore = await scoreService.getScore(req.headers.id)
 
-  if(getScore.error) {
-    res.status(400).send(getScore)
+  if (!req.headers.id) {
+    res.status(400).send({ error: "Il manque un paramètre dans le headers" });
   }
-  res.status(200).send(getScore)
-})
+  const getScore = await scoreService.getScore(req.headers.id);
+
+  if (getScore.error) {
+    res.status(400).send(getScore);
+  }
+  res.status(200).send(getScore);
+});
 
 app.listen(process.env.PORT || 3002, () => {
   console.log("Server app listening on port 3002");
